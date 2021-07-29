15df41d37d579e575dbcd8647d62283666237235
luoliver <luismonroy97@live.com>
2021-07-28 21:43:25 -0500

    LM - Se realiza cargue del controlador de TipoDocumento

---
+++ src/com/diplomado/bibliotheque/connaissances/controlador/TipoDocumentoController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diplomado.bibliotheque.connaissances.controlador;
+
+/**
+ *
+ * @author HP
+ */
+public class TipoDocumentoController {
+    
+}
